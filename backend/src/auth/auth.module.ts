d4f15b441f7525d1b29805b51ee84b622cb69a98
XyrelTenz <xdemocrito1@gmail.com>
2025-12-05 14:02:17 +0800

    Auth

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { PrismaModule } from "../../prisma/prisma.service";
+import { PrismaModule } from 'prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     PrismaModule,
     JwtModule.register({
-      global: true, // Makes JwtService available everywhere
-      secret: process.env.JWT_SECRET || 'secretKey123', // Use .env in production
-      signOptions: { expiresIn: '7d' }, // Token expires in 7 days
+      global: true,
+      secret: process.env.JWT_SECRET || 'secretKey123',
+      signOptions: { expiresIn: '7d' },
     }),
   ],
   controllers: [AuthController],
