8909a4e86a76f9dca0c69d1c86f5278f2543f965
XyrelTenz <xdemocrito1@gmail.com>
2025-12-10 21:41:00 +0800

    Borrowing DTO

--- backend/src/borrowing/dto/create-borrowing.dto.ts
+++ backend/src/borrowing/dto/create-borrowing.dto.ts
@@ -1,1 +1,12 @@
-export class CreateBorrowingDto {}
+import { IsInt, IsUUID, IsISO8601 } from 'class-validator';
+
+export class CreateBorrowDto {
+  @IsInt()
+  bookId: number;
+
+  @IsUUID()
+  userId: string;
+
+  @IsISO8601()
+  dueDate: string;
+}
