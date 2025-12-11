00794e56943b007fdb760e4f630b2445440b25f7
XyrelTenz <xdemocrito1@gmail.com>
2025-12-12 00:34:09 +0800

    Reconfig Routes to handle many Router

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,30 +1,12 @@
 import { Module } from '@nestjs/common';
-import { RouterModule } from '@nestjs/core';
 
-// Feature Modules
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { BorrowingModule } from './borrowing/borrowing.module';
-import { NotificationsModule } from './notifications/notifications.module';
-
-// Core/Shared Modules
-import { PrismaModule } from '.././prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
+import { RoutesModule } from './routes/routes.module';
 
 @Module({
   imports: [
     PrismaModule,
-
-    AuthModule,
-    UsersModule,
-    BorrowingModule,
-    NotificationsModule,
-
-    // RouterModule.register([
-    //   {
-    //     path: 'library', 
-    //     module: BookModules,
-    //   },
-    // ]),
+    RoutesModule,
   ],
 })
 export class AppModule { }
---
+++ backend/src/routes/routes.module.ts
@@ -0,0 +1,41 @@
+import { Module } from '@nestjs/common';
+import { RouterModule } from '@nestjs/core';
+
+import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
+import { BorrowingModule } from '../borrowing/borrowing.module';
+import { NotificationsModule } from '../notifications/notifications.module';
+
+@Module({
+  imports: [
+    AuthModule,
+    UsersModule,
+    BorrowingModule,
+    NotificationsModule,
+
+    RouterModule.register([
+      {
+        path: 'api',
+        children: [
+          {
+            path: 'auth',
+            module: AuthModule,
+          },
+          {
+            path: 'users',
+            module: UsersModule,
+          },
+          {
+            path: 'borrowing',
+            module: BorrowingModule,
+          },
+          {
+            path: 'notifications',
+            module: NotificationsModule,
+          },
+        ],
+      },
+    ]),
+  ],
+})
+export class RoutesModule { }
