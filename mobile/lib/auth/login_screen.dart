dd594e5b96e4b30dc6fff758bf10938c07963e77
XyrelTenz <xdemocrito1@gmail.com>
2025-12-20 21:24:43 +0800

    Auth Login

---
+++ mobile/lib/auth/login_screen.dart
@@ -0,0 +1,25 @@
+import "package:flutter/material.dart";
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Login Here")));
+  }
+}
